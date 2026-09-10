8de94159a1d90ab63c73db25f2799c2e1f48e7c2
butterChen <buttercsub@gmail.com>
2026-09-11 00:07:12 +0800

    md調整
    md檔案調整
    花錢的藝術 調整

--- quartz.config.ts
+++ quartz.config.ts
@@ -45,6 +45,9 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
+      Plugin.MarkdownPlugins({
+        remarkPlugins: [remarkBreaks] // 2. 在這裡加入 remarkBreaks
+      }),
       Plugin.TableOfContents(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
